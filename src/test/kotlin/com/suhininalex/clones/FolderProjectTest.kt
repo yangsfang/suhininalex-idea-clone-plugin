4f3630d953cde13439b880768e60785ae5a7fc20
yangsfang <31492969+yangsfang@users.noreply.github.com>
2024-02-16 20:18:38 -0800

    Ignore empty test

--- src/test/kotlin/com/suhininalex/clones/FolderProjectTest.kt
+++ src/test/kotlin/com/suhininalex/clones/FolderProjectTest.kt
@@ -4,13 +4,15 @@ import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.impl.source.tree.ElementType
 import com.intellij.psi.tree.TokenSet
-import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import com.suhininalex.clones.core.*
 import com.suhininalex.clones.core.languagescope.java.JavaIndexedSequence
 import com.suhininalex.clones.core.utils.findTokens
+import org.junit.Ignore
 import kotlin.properties.Delegates
 
-open class FolderProjectTest(val testFolder: String) : LightCodeInsightFixtureTestCase() {
+@Ignore("WIP")
+open class FolderProjectTest(val testFolder: String) : LightJavaCodeInsightFixtureTestCase() {
 
     var baseDirectoryPsi by Delegates.notNull<PsiDirectory>()
 
@@ -25,5 +27,4 @@ open class FolderProjectTest(val testFolder: String) : LightCodeInsightFixtureTe
 //                CloneIndexer.addSequence(JavaIndexedSequence(method))
 //        }
     }
-
 }
